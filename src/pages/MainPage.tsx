002dd817dc471aa7582e0f53e0ff29c8bfdca709
ralph <ralph0813.ljt@gmail.com>
2021-12-01 21:07:37 +0800

    添加今日练习

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import WFD from './WFD'
 
 const MainPage = (): JSX.Element => (
   <div className="min-h-screen-ios p-3 sm:p-8 md:p-12 lg:p-16 justify-center">
-    <div className='bg-gray-50 rounded-lg p-5 sm:p-8 md:p-12 lg:p-16'>
+    <div className='rounded-lg p-5 sm:p-8 md:p-12 lg:p-16'>
       <WFD />
     </div>
   </div>
--- src/pages/WFD.tsx
+++ src/pages/WFD.tsx
@@ -6,8 +6,8 @@ import AddSentence from '../components/wfd/AddSentence'
 import { listInitialState, listReducer } from '../reducers/sentenceReducer'
 
 const WFD = (): JSX.Element => {
-  const [list, listDispatcher] = React.useReducer(listReducer, listInitialState)
-  const total = list?.length || 0
+  const [sentenceInfoList, listDispatcher] = React.useReducer(listReducer, listInitialState)
+  const total = sentenceInfoList?.length || 0
 
   const dispatchAddSentence = (sentence: string) => {
     listDispatcher({
@@ -33,32 +33,54 @@ const WFD = (): JSX.Element => {
   }, [])
 
   const practicedTodayList = React.useMemo(() => {
-    return list.filter((item: SentenceInfo) => item.practicedToday)
-  }, [list])
+    return sentenceInfoList.filter((item: SentenceInfo) => item.practicedToday)
+  }, [sentenceInfoList])
 
   return (
     <div className="flex flex-col space-y-5">
       <div className="flex items-center justify-between">
-        <div className="text-3xl font-bold">WFD</div>
+        <div className="text-3xl font-bold text-gray-900">PTE Practice - WFD</div>
       </div>
       <div className="text-xl">{` 一共有: ${total} 句哦！今日已经练习: ${practicedTodayList.length} 句！`}</div>
       <div>
         <AddSentence dispatchAddSentence={dispatchAddSentence} />
       </div>
-      <div className="space-y-2">
-        {list?.length !== 0 ? list.map((item) => (
-          <WfdItem
-            item={item} key={item.id}
-            dispatchDelSentence={dispatchDelSentence}
-            dispatchPracSentence={dispatchPracSentence}
-          />
-        )) : (
-          <div className="text-center py-10 bg-gray-50 rounded">
-            <CubeTransparentIcon className="mx-auto h-12 w-12 text-gray-400" />
-            <h3 className="mt-2 text-sm font-medium text-gray-900">No sentences.</h3>
-            <p className="mt-1 text-sm text-gray-500">No sentence need to be practiced!.</p>
-          </div>
-        )}
+
+      <div className='border p-3 rounded shadow-sm'>
+        <div className="text-xl pb-2 font-bold text-gray-800">今日已练习：</div>
+        <div className="space-y-2">
+          {practicedTodayList?.length !== 0 ? practicedTodayList.map((item) => (
+            <WfdItem
+              item={item} key={item.id}
+              dispatchDelSentence={dispatchDelSentence}
+              dispatchPracSentence={dispatchPracSentence}
+            />
+          )) : (
+            <div className="text-center py-10 bg-gray-50 rounded">
+              <CubeTransparentIcon className="mx-auto h-12 w-12 text-gray-400" />
+              <h3 className="mt-2 text-sm font-medium text-gray-900">No sentences.</h3>
+              <p className="mt-1 text-sm text-gray-500">No sentence has been practiced today!</p>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className='border p-3 rounded shadow-sm'>
+        <div className="text-xl pb-2 font-bold text-gray-800">所有句子：</div>
+        <div className="space-y-2">
+          {sentenceInfoList?.length !== 0 ? sentenceInfoList.map((item) => (
+            <WfdItem
+              item={item} key={item.id}
+              dispatchDelSentence={dispatchDelSentence}
+              dispatchPracSentence={dispatchPracSentence}
+            />
+          )) : (
+            <div className="text-center py-10 bg-gray-50 rounded">
+              <CubeTransparentIcon className="mx-auto h-12 w-12 text-gray-400" />
+              <h3 className="mt-2 text-sm font-medium text-gray-900">No sentences.</h3>
+              <p className="mt-1 text-sm text-gray-500">Add some sentences first.</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
